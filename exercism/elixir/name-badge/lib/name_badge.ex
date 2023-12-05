fdf0955f1a72bf088a9e4483173ab56ef54b57a1
MmmarRTHa <m27842n@outlook.com>
2023-12-04 18:16:54 -0800

    - update: main - removed duplicate files.

--- exercism/elixir/name-badge/lib/name_badge.ex
+++
@@ -1,8 +0,0 @@
-defmodule NameBadge do
-    def print(id, name, department) do
-        id_string = if id, do: "[#{id}] - ", else: nil
-
-        dept_string = if department, do: " - #{String.upcase(department)}", else: " - OWNER"
-        "#{id_string}#{name}#{dept_string}"
-    end
-end
