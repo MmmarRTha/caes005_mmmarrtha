d6ddee09f2de26abc1dcd01414bdbb92a712012b
MmmarRTHa <m27842n@outlook.com>
2023-12-13 14:52:15 -0800

    - update: feature/tic_tac_toe_4 - added started files.

---
+++ projects/tic_tac_toe_4/lib/tic_tac_toe4.ex
@@ -0,0 +1,18 @@
+defmodule TicTacToe4 do
+  @moduledoc """
+  Documentation for `TicTacToe4`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TicTacToe4.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
